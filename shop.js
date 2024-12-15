06dbe197cdf34656da39890deb0d746c70dac5d3
Dhruva V <dhruva.v@somaiya.edu>
2024-12-15 15:53:29 +0530

    links connected properly

---
+++ shop.js
@@ -0,0 +1,41 @@
+var buttonsContainer = document.getElementById("but-1");
+if (buttonsContainer) {
+  buttonsContainer.addEventListener("click", function (e) {
+    window.location.href = "plastic-waste.html";
+  });
+}
+
+var buttonsContainer = document.getElementById("but-2");
+if (buttonsContainer) {
+  buttonsContainer.addEventListener("click", function (e) {
+    window.location.href = "./paper-waste.html";
+  });
+}
+
+var button1 = document.getElementById("but-3");
+if (button1) {
+  button1.addEventListener("click", function (e) {
+    window.location.href = "./metal-waste.html";
+  });
+}
+
+var buttonsContainer = document.getElementById("but-4");
+if (buttonsContainer) {
+  buttonsContainer.addEventListener("click", function (e) {
+    window.location.href = "./electronic-waste.html";
+  });
+}
+
+var buttonsContainer = document.getElementById("but-5");
+if (buttonsContainer) {
+  buttonsContainer.addEventListener("click", function (e) {
+    window.location.href = "./other-waste.html";
+  });
+}
+
+var buttonsContainer = document.getElementById("shop-home");
+if (buttonsContainer) {
+  buttonsContainer.addEventListener("click", function (e) {
+    window.location.href = "Home.html";
+  });
+}
